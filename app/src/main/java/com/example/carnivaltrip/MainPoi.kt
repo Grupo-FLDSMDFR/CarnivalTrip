6700e8c11cfe7f597b80d60a29f18f1bc7ec1892
WHURTADOH <wilton.ahurtado@gmail.com>
2021-09-15 19:29:11 -0500

    Se crea la nueva vista

---
+++ app/src/main/java/com/example/carnivaltrip/MainPoi.kt
@@ -0,0 +1,11 @@
+package com.example.carnivaltrip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainPoi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_poi)
+    }
+}
