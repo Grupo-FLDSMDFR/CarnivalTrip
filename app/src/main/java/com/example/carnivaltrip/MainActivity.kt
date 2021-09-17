82fecd857605879ce909000069d020cf745e284f
WHURTADOH <wilton.ahurtado@gmail.com>
2021-09-16 22:28:29 -0500

    Finaliza Recyclerview

--- app/src/main/java/com/example/carnivaltrip/MainActivity.kt
+++ app/src/main/java/com/example/carnivaltrip/MainActivity.kt
@@ -2,19 +2,14 @@ package com.example.carnivaltrip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-    val poi = listOf(
-        ListPoi("Melbourne", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan ipsum sit amet nisl ultricies maximus. Morbi finibus urna a neque maximus, ac sagittis orci faucibus. Nunc congue lorem nunc.", "10ptos", "https://gostudyaus.es/wp-content/uploads/2018/10/MEL_header_2-1.jpg"),
-        ListPoi("Melbourne", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan ipsum sit amet nisl ultricies maximus. Morbi finibus urna a neque maximus, ac sagittis orci faucibus. Nunc congue lorem nunc.", "10ptos", "https://gostudyaus.es/wp-content/uploads/2018/10/MEL_header_2-1.jpg"),
-        ListPoi("Melbourne", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan ipsum sit amet nisl ultricies maximus. Morbi finibus urna a neque maximus, ac sagittis orci faucibus. Nunc congue lorem nunc.", "10ptos", "https://gostudyaus.es/wp-content/uploads/2018/10/MEL_header_2-1.jpg"),
-        ListPoi("Melbourne", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan ipsum sit amet nisl ultricies maximus. Morbi finibus urna a neque maximus, ac sagittis orci faucibus. Nunc congue lorem nunc.", "10ptos", "https://gostudyaus.es/wp-content/uploads/2018/10/MEL_header_2-1.jpg"),
-        ListPoi("Melbourne", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan ipsum sit amet nisl ultricies maximus. Morbi finibus urna a neque maximus, ac sagittis orci faucibus. Nunc congue lorem nunc.", "10ptos", "https://gostudyaus.es/wp-content/uploads/2018/10/MEL_header_2-1.jpg")
-    )
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import kotlinx.android.synthetic.main.activity_main_poi.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
+
 }
--- app/src/main/java/com/example/carnivaltrip/MainPoi.kt
+++ app/src/main/java/com/example/carnivaltrip/MainPoi.kt
@@ -2,10 +2,27 @@ package com.example.carnivaltrip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main_poi.*
+
+val poi = listOf(
+    ListPoi("Melbourne", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan ipsum sit amet nisl ultricies maximus. Morbi finibus urna a neque maximus, ac sagittis orci faucibus. Nunc congue lorem nunc.", "10ptos", "https://gostudyaus.es/wp-content/uploads/2018/10/MEL_header_2-1.jpg"),
+    ListPoi("Melbourne", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan ipsum sit amet nisl ultricies maximus. Morbi finibus urna a neque maximus, ac sagittis orci faucibus. Nunc congue lorem nunc.", "10ptos", "https://gostudyaus.es/wp-content/uploads/2018/10/MEL_header_2-1.jpg"),
+    ListPoi("Melbourne", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan ipsum sit amet nisl ultricies maximus. Morbi finibus urna a neque maximus, ac sagittis orci faucibus. Nunc congue lorem nunc.", "10ptos", "https://gostudyaus.es/wp-content/uploads/2018/10/MEL_header_2-1.jpg"),
+    ListPoi("Melbourne", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan ipsum sit amet nisl ultricies maximus. Morbi finibus urna a neque maximus, ac sagittis orci faucibus. Nunc congue lorem nunc.", "10ptos", "https://gostudyaus.es/wp-content/uploads/2018/10/MEL_header_2-1.jpg"),
+    ListPoi("Melbourne", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan ipsum sit amet nisl ultricies maximus. Morbi finibus urna a neque maximus, ac sagittis orci faucibus. Nunc congue lorem nunc.", "10ptos", "https://gostudyaus.es/wp-content/uploads/2018/10/MEL_header_2-1.jpg")
+)
 
 class MainPoi : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_poi)
+        initRecycler()
+    }
+
+    fun initRecycler(){
+        r_list_poi.layoutManager = LinearLayoutManager(this)
+        val adapter = PoiAdapter(poi)
+        r_list_poi.adapter=adapter
     }
 }
