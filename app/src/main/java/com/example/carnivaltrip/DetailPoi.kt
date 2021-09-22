c050290db8955db17ee40d688edff0aea41b69b8
WHURTADOH <wilton.ahurtado@gmail.com>
2021-09-22 15:26:02 -0500

    Implementacion de los fragments

---
+++ app/src/main/java/com/example/carnivaltrip/DetailPoi.kt
@@ -0,0 +1,13 @@
+package com.example.carnivaltrip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main_poi.*
+
+class DetailPoi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+
+}
